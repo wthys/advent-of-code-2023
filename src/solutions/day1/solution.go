fd930a840dcc5b5b07b0904953bec440e1bbcd61
Wim Thys <wim.thys@zardof.be>
2023-12-01 10:36:34 +0100

    day 1 part 1

--- src/solutions/day1/solution.go
+++ src/solutions/day1/solution.go
@@ -1,6 +1,9 @@
 package day1
 
 import (
+    "fmt"
+    "regexp"
+    "strconv"
     "github.com/wthys/advent-of-code-2023/solver"
 )
 
@@ -16,7 +19,21 @@ func (s solution) Day() string {
 }
 
 func (s solution) Part1(input []string) (string, error) {
-    return solver.NotImplemented()
+    re := regexp.MustCompile("[0-9]")
+
+    total := 0
+
+    for _, line := range input {
+        matches := re.FindAllString(line, -1)
+        
+        if (len(matches) > 0) {
+            cand, _ := strconv.Atoi(matches[0] + matches[len(matches)-1])
+            total = total + cand
+        }
+    }
+
+
+    return solver.Solved(total)
 }
 
 func (s solution) Part2(input []string) (string, error) {
