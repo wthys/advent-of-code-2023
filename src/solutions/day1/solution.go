fe6179c1e52f6f0bf9857a151588fa2c7275e7ee
Wim Thys <wim.thys@zardof.be>
2023-12-01 13:10:39 +0100

    cleaned up day 1

--- src/solutions/day1/solution.go
+++ src/solutions/day1/solution.go
@@ -1,9 +1,6 @@
 package day1
 
 import (
-	"fmt"
-	"regexp"
-	"strconv"
 	"strings"
 
 	"github.com/wthys/advent-of-code-2023/solver"
@@ -19,50 +16,20 @@ func (s solution) Day() string {
 	return "1"
 }
 
-var strMapping map[string]int = map[string]int{
-	"one":   1,
-	"two":   2,
-	"three": 3,
-	"four":  4,
-	"five":  5,
-	"six":   6,
-	"seven": 7,
-	"eight": 8,
-	"nine":  9,
-	"1":     1,
-	"2":     2,
-	"3":     3,
-	"4":     4,
-	"5":     5,
-	"6":     6,
-	"7":     7,
-	"8":     8,
-	"9":     9,
-}
-
-func convertNumber(input string) (int, error) {
-	result, ok := strMapping[input]
-	if !ok {
-		return 0, fmt.Errorf("unknown number [%s]", input)
-	}
-
-	return result, nil
-}
-
-func extractNumber(input string) int {
+func extractNumber(input string, mapping map[string]int) int {
 	index1 := len(input)
 	dig1 := 0
 	index2 := -1
 	dig2 := 0
-	for key, value := range strMapping {
+	for key, value := range mapping {
 		frIdx := strings.Index(input, key)
-		if frIdx >= 0 && frIdx < index1 {
-			index1 = frIdx
+		if frIdx >= 0 {
+			index1 = min(frIdx, index1)
 			dig1 = value
 		}
 		laIdx := strings.LastIndex(input, key)
-		if laIdx >= 0 && laIdx+len(key) > index2 {
-			index2 = laIdx + len(key)
+		if laIdx >= 0 {
+			index2 = max(laIdx+len(key), index2)
 			dig2 = value
 		}
 	}
@@ -71,17 +38,13 @@ func extractNumber(input string) int {
 }
 
 func (s solution) Part1(input []string) (string, error) {
-	re := regexp.MustCompile("[0-9]")
-
 	total := 0
+	mapping := map[string]int{
+		"1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
+	}
 
 	for _, line := range input {
-		matches := re.FindAllString(line, -1)
-
-		if len(matches) > 0 {
-			cand, _ := strconv.Atoi(matches[0] + matches[len(matches)-1])
-			total = total + cand
-		}
+		total += extractNumber(line, mapping)
 	}
 
 	return solver.Solved(total)
@@ -89,10 +52,13 @@ func (s solution) Part1(input []string) (string, error) {
 
 func (s solution) Part2(input []string) (string, error) {
 	total := 0
+	mapping := map[string]int{
+		"1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
+		"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9,
+	}
 
 	for _, line := range input {
-		cand := extractNumber(line)
-		total += cand
+		total += extractNumber(line, mapping)
 	}
 
 	return solver.Solved(total)
