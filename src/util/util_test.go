c1dce5d1bf2113409bbb5d2af2de3ba40a3b5dfb
Wim Thys <wim.thys@zardof.be>
2023-12-01 12:54:20 +0100

    add util.Do variants + tests

--- src/util/util_test.go
+++ src/util/util_test.go
@@ -1,118 +1,195 @@
 package util
 
 import (
-    "fmt"
-    "testing"
+	"fmt"
+	"testing"
 )
 
 type (
-    testInt struct {
-        Input int
-        Want int
-    }
+	testInt struct {
+		Input int
+		Want  int
+	}
 
-    testFloat struct {
-        Input float64
-        Want float64
-    }
+	testFloat struct {
+		Input float64
+		Want  float64
+	}
 )
 
 func TestSignInt(t *testing.T) {
-    cases := []testInt{
-        {int(5), int(1)},
-        {int(0), int(0)},
-        {int(-245), int(-1)},
-    }
+	cases := []testInt{
+		{int(5), int(1)},
+		{int(0), int(0)},
+		{int(-245), int(-1)},
+	}
 
-    for _, cs := range cases {
-        t.Run(fmt.Sprintf("%v", cs.Input), func(t *testing.T) {
-            s := Sign(cs.Input)
-            if s != cs.Want {
-                t.Fatalf("Sign(%v) = %v, want %v", cs.Input, s, cs.Want)
-            }
-        })
-    }
+	for _, cs := range cases {
+		t.Run(fmt.Sprintf("%v", cs.Input), func(t *testing.T) {
+			s := Sign(cs.Input)
+			if s != cs.Want {
+				t.Fatalf("Sign(%v) = %v, want %v", cs.Input, s, cs.Want)
+			}
+		})
+	}
 }
 
 func TestSignFloat(t *testing.T) {
-    cases := []testFloat{
-        {float64(3.14), float64(1.0)},
-        {float64(0.0), float64(0.0)},
-        {float64(-5.256), float64(-1.0)},
-    }
+	cases := []testFloat{
+		{float64(3.14), float64(1.0)},
+		{float64(0.0), float64(0.0)},
+		{float64(-5.256), float64(-1.0)},
+	}
 
-    for _, cs := range cases {
-        t.Run(fmt.Sprintf("%v", cs.Input), func(t *testing.T) {
-            s := Sign(cs.Input)
-            if s != cs.Want {
-                t.Fatalf("Sign(%v) = %v, want %v", cs.Input, s, cs.Want)
-            }
-        })
-    }
+	for _, cs := range cases {
+		t.Run(fmt.Sprintf("%v", cs.Input), func(t *testing.T) {
+			s := Sign(cs.Input)
+			if s != cs.Want {
+				t.Fatalf("Sign(%v) = %v, want %v", cs.Input, s, cs.Want)
+			}
+		})
+	}
 }
 
 func TestAbsInt(t *testing.T) {
-    cases := []testInt{
-        {int(5), int(5)},
-        {int(0), int(0)},
-        {int(-245), int(245)},
-    }
+	cases := []testInt{
+		{int(5), int(5)},
+		{int(0), int(0)},
+		{int(-245), int(245)},
+	}
 
-    for _, cs := range cases {
-        t.Run(fmt.Sprintf("%v", cs.Input), func(t *testing.T) {
-            s := Abs(cs.Input)
-            if s != cs.Want {
-                t.Fatalf("Abs(%v) = %v, want %v", cs.Input, s, cs.Want)
-            }
-        })
-    }
+	for _, cs := range cases {
+		t.Run(fmt.Sprintf("%v", cs.Input), func(t *testing.T) {
+			s := Abs(cs.Input)
+			if s != cs.Want {
+				t.Fatalf("Abs(%v) = %v, want %v", cs.Input, s, cs.Want)
+			}
+		})
+	}
 }
 
 func TestAbsFloat(t *testing.T) {
-    cases := []testFloat{
-        {float64(3.14), float64(3.14)},
-        {float64(0.0), float64(0.0)},
-        {float64(-5.256), float64(5.256)},
-    }
+	cases := []testFloat{
+		{float64(3.14), float64(3.14)},
+		{float64(0.0), float64(0.0)},
+		{float64(-5.256), float64(5.256)},
+	}
 
-    for _, cs := range cases {
-        t.Run(fmt.Sprintf("%v", cs.Input), func(t *testing.T) {
-            s := Abs(cs.Input)
-            if s != cs.Want {
-                t.Fatalf("Abs(%v) = %v, want %v", cs.Input, s, cs.Want)
-            }
-        })
-    }
+	for _, cs := range cases {
+		t.Run(fmt.Sprintf("%v", cs.Input), func(t *testing.T) {
+			s := Abs(cs.Input)
+			if s != cs.Want {
+				t.Fatalf("Abs(%v) = %v, want %v", cs.Input, s, cs.Want)
+			}
+		})
+	}
 }
 
 func hash(array []int) int {
-    h := 0
-    for _, value := range array {
-        h = 13*h + value
-    }
-    return h
+	h := 0
+	for _, value := range array {
+		h = 13*h + value
+	}
+	return h
 }
 
 func TestPermutationDo(t *testing.T) {
-    array := []int{1,2,3}
+	array := []int{1, 2, 3}
 
-    check := map[int]bool{
-        hash([]int{1,2,3}): false,
-        hash([]int{1,3,2}): false,
-        hash([]int{2,1,3}): false,
-        hash([]int{2,3,1}): false,
-        hash([]int{3,2,1}): false,
-        hash([]int{3,1,2}): false,
-    }
+	check := map[int]bool{
+		hash([]int{1, 2, 3}): false,
+		hash([]int{1, 3, 2}): false,
+		hash([]int{2, 1, 3}): false,
+		hash([]int{2, 3, 1}): false,
+		hash([]int{3, 2, 1}): false,
+		hash([]int{3, 1, 2}): false,
+	}
 
-    PermutationDo(3, array, func(perm []int) {
-        check[hash(perm)] = true
-    })
+	PermutationDo(3, array, func(perm []int) {
+		check[hash(perm)] = true
+	})
 
-    for value, seen := range check {
-        if !seen {
-            t.Fatalf("PermutationDo(3, %v, ...) should produce %v, but was not seen", array, value)
-        }
-    }
+	for value, seen := range check {
+		if !seen {
+			t.Fatalf("PermutationDo(3, %v, ...) should produce %v, but was not seen", array, value)
+		}
+	}
 
+}
+
+func TestDo(t *testing.T) {
+	array := []int{1, 2, 3}
+	check := map[int]bool{
+		1: false,
+		2: false,
+		3: false,
+	}
+
+	Do(array, func(value int) {
+		check[value] = true
+	})
+
+	for value, seen := range check {
+		if !seen {
+			t.Fatalf("Do(%v, ...) should produce %v, but was not seen", array, value)
+		}
+	}
+}
+
+func TestDoWithError(t *testing.T) {
+	array := []int{1, 2, 3, 4, 5}
+	check := map[int]bool{
+		1: false,
+		2: false,
+		3: false,
+		4: false,
+		5: false,
+	}
+
+	err := DoWithError(array, func(value int) error {
+		if value > 3 {
+			return fmt.Errorf("value %v is too large", value)
+		}
+		check[value] = true
+		return nil
+	})
+
+	if err == nil {
+		t.Fatalf("DoWithError(%v, ...) should produce an error but none was seen", array)
+	}
+
+	for value, seen := range check {
+		if !seen && value <= 3 {
+			t.Fatalf("Do(%v, ...) should produce %v, but was not seen", array, value)
+		}
+	}
+}
+
+func TestDoContinue(t *testing.T) {
+	array := []int{1, 2, 3, 4, 5}
+	check := map[int]bool{
+		1: false,
+		2: false,
+		3: false,
+		4: false,
+		5: false,
+	}
+
+	continued := DoContinue(array, func(value int) bool {
+		if value > 3 {
+			return false
+		}
+		check[value] = true
+		return true
+	})
+
+	if continued {
+		t.Fatalf("DoContinue(%v, ...) should produce %v but was %v", array, false, continued)
+	}
+
+	for value, seen := range check {
+		if !seen && value <= 3 {
+			t.Fatalf("Do(%v, ...) should produce %v, but was not seen", array, value)
+		}
+	}
 }
