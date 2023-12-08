856b5cff4e657a2252cbcd2d9298e8f41c294a04
Wim Thys <wim.thys@zardof.be>
2023-12-08 15:00:20 +0100

    day 8 part 2 solution

--- src/solutions/day8/solution.go
+++ src/solutions/day8/solution.go
@@ -3,8 +3,10 @@ package day8
 import (
 	"fmt"
 	"regexp"
+	"slices"
 
 	"github.com/wthys/advent-of-code-2023/solver"
+	"github.com/wthys/advent-of-code-2023/util"
 )
 
 type solution struct{}
@@ -24,8 +26,10 @@ func (s solution) Part1(input []string) (string, error) {
 	}
 
 	step := 0
-	current := Node("AAA")
-	target := Node("ZZZ")
+	nodes := desertMap.Nodes()
+	slices.Sort(nodes)
+	current := nodes[0]
+	target := nodes[len(nodes)-1]
 	for current != target {
 		next, ok := desertMap.Next(current, step)
 		if !ok {
@@ -39,7 +43,66 @@ func (s solution) Part1(input []string) (string, error) {
 }
 
 func (s solution) Part2(input []string) (string, error) {
-	return solver.NotImplemented()
+	desertMap, err := ParseInput(input)
+	if err != nil {
+		return solver.Error(err)
+	}
+
+	reStart := regexp.MustCompile(`A$`)
+	reEnd := regexp.MustCompile(`Z$`)
+
+	endStates := []int{}
+	diffs := []int{}
+	current := Nodes{}
+	desertMap.Nodes().ForEach(func(node Node) {
+		if reStart.MatchString(string(node)) {
+			current = append(current, node)
+			endStates = append(endStates, 0)
+			diffs = append(diffs, 0)
+		}
+	})
+
+	step := 0
+	limit := 50000
+	stopped := true
+	for step < limit {
+		next := Nodes{}
+		ended := true
+		for idx, node := range current {
+			nn, ok := desertMap.Next(node, step)
+			if !ok {
+				return solver.Error(fmt.Errorf("could not find next node for %v", node))
+			}
+			next = append(next, nn)
+			if reEnd.MatchString(string(nn)) {
+				diff := step - endStates[idx]
+				diffs[idx] = diff
+				limit = max(limit, 5*diff)
+				endStates[idx] = step
+			} else {
+				ended = false
+			}
+		}
+		current = next
+		step += 1
+		if ended {
+			stopped = false
+			break
+		}
+	}
+
+	if stopped {
+		switch len(diffs) {
+		case 1:
+			step = diffs[0]
+		case 2:
+			step = util.LCM(diffs[0], diffs[1])
+		default:
+			step = util.LCM(diffs[0], diffs[1], diffs[2:]...)
+		}
+	}
+
+	return solver.Solved(step)
 }
 
 type (
@@ -71,6 +134,20 @@ func (m Map) Next(node Node, step int) (Node, bool) {
 	return next, true
 }
 
+func (m Map) Nodes() Nodes {
+	nodes := Nodes{}
+	for key, _ := range m.nodes {
+		nodes = append(nodes, key)
+	}
+	return nodes
+}
+
+func (nodes Nodes) ForEach(forEach func(Node)) {
+	for _, node := range nodes {
+		forEach(node)
+	}
+}
+
 func ParseInput(input []string) (Map, error) {
 	if len(input) < 3 {
 		return Map{}, fmt.Errorf("not enough input")
@@ -85,7 +162,7 @@ func ParseInput(input []string) (Map, error) {
 	}
 
 	nodes := NodeMap{}
-	reNodeMap := regexp.MustCompile(`^\s*([A-Z]+)\s*=\s*[(]\s*([A-Z]+)\s*,\s*([A-Z]+)\s*[)]\s*$`)
+	reNodeMap := regexp.MustCompile(`^\s*([0-9A-Z]+)\s*=\s*[(]\s*([0-9A-Z]+)\s*,\s*([0-9A-Z]+)\s*[)]\s*$`)
 
 	for lineNr, line := range input[2:] {
 		matches := reNodeMap.FindStringSubmatch(line)
